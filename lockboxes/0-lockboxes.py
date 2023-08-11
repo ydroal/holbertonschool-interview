085e2c03cfb24423120873d420299e5924c7c1db
ydroal <droal.yoko@gmail.com>
2023-08-11 17:41:53 +0200

    add: task0

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+This module is a script to determines if all the boxes can be opened.
+
+Args:
+  boxes (list): list of lists
+'''
+
+
+def canUnlockAll(boxes):
+    length = len(boxes)
+    if not boxes:
+        return False
+    if length == 1:
+        return True
+    if not boxes[0]:
+        return False
+    # 探索キューを作成
+    queue = list(boxes[0])
+    opened_boxes = {0}
+
+    while queue:
+        popped_key = queue.pop(0)
+        if popped_key not in opened_boxes:
+            opened_boxes.add(popped_key)
+            collected_keys = list(boxes[popped_key])
+            for key in collected_keys:
+                if key not in opened_boxes:
+                    queue.append(key)
+
+    box_list = set(range(length))
+
+    return opened_boxes == box_list
