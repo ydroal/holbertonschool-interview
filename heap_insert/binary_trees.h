24f4b3c1e9ecf596f570ec0dcdd5ecc03b8073a4
ydroal <droal.yoko@gmail.com>
2023-08-28 11:06:26 +0000

    add: task0

---
+++ heap_insert/binary_trees.h
@@ -0,0 +1,40 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+//------------------------------------------------
+// Type definition
+//------------------------------------------------
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+typedef struct binary_tree_s heap_t;
+
+//------------------------------------------------
+//  Prototype declaration
+//------------------------------------------------
+
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif /* BINARY_TREES_H */
+
