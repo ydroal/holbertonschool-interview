1f1eef7d7332c261ec0e8831e353afe8f72cb09d
ydroal <droal.yoko@gmail.com>
2023-09-07 12:30:46 +0200

    fix: PEP-8

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,6 +3,7 @@
 Module to calculate the fewest number of operations
 '''
 
+
 def minOperations(n):
     '''
     calculates the fewest number of operations needed to
@@ -10,23 +11,21 @@ def minOperations(n):
 
     Args:
     n (int): Number of 'H' characters to display
-    
+
     Returns:
     int: fewest number of operations
     If n is impossible to achieve, return 0
     '''
-    
+
     if n < 2:
         return 0
-    
+
     ope = 0
     factor = 2
-    
+
     while n > 1:
         while n % factor == 0:
-            ope += factor # 素因数の値を加算
+            ope += factor  # 素因数の値を加算
             n //= factor
-        factor += 1 # 次の素数
+        factor += 1  # 次の素数
     return ope
-    
-    
