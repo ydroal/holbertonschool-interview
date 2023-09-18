d7636e01a5c7edd5618ce74b96a8abe7e8fc8e6b
ydroal <droal.yoko@gmail.com>
2023-09-18 13:40:51 +0000

    fix: task0

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -6,13 +6,12 @@
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 
-
 int is_palindrome(listint_t **head)
 {
 	listint_t *current = *head;
 	int *stack;
 	int length = 0; /* ノードの数 */
-	int push_limit = length / 2;
+	int push_limit;
 	int i;
 
 	if (!head || !*head || !(*head)->next)
@@ -23,11 +22,12 @@ int is_palindrome(listint_t **head)
 		current = current->next;
 		length++;
 	}
-
+	push_limit = length / 2;
 	stack = (int *)malloc(length * sizeof(int));  /* メモリ領域の確保 */
 	if (stack == NULL)
 		return (0);
 
+	current = *head; /* currentをリセット */
 	for (i = 0; i < push_limit; i++)
 	{
 		stack[i] = current->n;
@@ -39,7 +39,7 @@ int is_palindrome(listint_t **head)
 
 	for (i = push_limit - 1; i >= 0; i--)
 	{
-		if (stack[i] != current->n)
+		if (current == NULL || stack[i] != current->n)
 		{
 			free(stack);
 			return (0);
