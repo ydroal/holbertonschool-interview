e2ffe47443f0dea6c0e2a011e1ba123876e74a53
ydroal <droal.yoko@gmail.com>
2023-09-19 09:46:34 +0000

    fix: Replace stack with reversed half list for palindrome checking

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -8,45 +8,44 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
-	int *stack;
-	int length = 0; /* ノードの数 */
-	int push_limit;
-	int i;
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev, *next, *second_half;
 
 	if (!head || !*head || !(*head)->next)
 		return (1);
 
-	while (current != NULL)
+	/* スタックのサイズを測定（リストの半分） */
+	while (fast != NULL && fast->next != NULL)
 	{
-		current = current->next;
-		length++;
+		fast = fast->next->next; /* 2ノードずつ(fastが末端時slowは中間)*/
+		slow = slow->next; /* slowは1ノードずつ進める */
 	}
-	push_limit = length / 2;
-	stack = (int *)malloc(push_limit * sizeof(int));  /* メモリ領域の確保 */
-	if (stack == NULL)
-		return (0);
+	/* 偶数か奇数かで後半のノード探索スタート位置を決定 */
+	if (fast != NULL) /* リストが奇数 */
+		second_half = slow->next;
+	else
+		second_half = slow;
 
-	current = *head; /* currentをリセット */
-	for (i = 0; i < push_limit; i++)
+	/* 後半部分のリストを逆転させる */
+	prev = NULL;
+	while (second_half != NULL)
 	{
-		stack[i] = current->n;
-		current = current->next;
+		next = second_half->next;
+		second_half->next = prev;
+		prev = second_half;
+		second_half = next;
 	}
-
-	if (length % 2 != 0)
-		current = current->next;
-
-	for (i = push_limit - 1; i >= 0; i--)
+	second_half = prev; /* 逆転させたリストの先頭をささせる */
+	next = *head; /* 元のリストの先頭をささせる */
+	while (second_half != NULL)
 	{
-		if (current == NULL || stack[i] != current->n)
+		if (next->n != second_half->n)
 		{
-			free(stack);
 			return (0);
 		}
-		current = current->next;
+		next = next->next;
+		second_half = second_half->next;
 	}
-	free(stack);
 	return (1);
 }
-
