8458f0613bbe22479ed50c7872f2bfe73172a4cd
ydroal <droal.yoko@gmail.com>
2023-09-28 17:21:52 +0200

    update: add __main__ guard

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -2,12 +2,6 @@
 """Module to read from stdin and compute metrics"""
 import sys
 
-total_size = 0
-status_codes = {'200': 0, '301': 0,
-                '400': 0, '401': 0,
-                '403': 0, '404': 0,
-                '405': 0, '500': 0}
-
 
 def parse_line(line):
     """
@@ -18,7 +12,7 @@ def parse_line(line):
 
     Returns:
     tuple: A tuple containing the IP address, request type,
-           status code, and file size.
+        status code, and file size.
     """
     try:
         # IPアドレスを取得
@@ -34,30 +28,40 @@ def parse_line(line):
         return ip, request, status, size
     except Exception:
         return None, None, None, None
+    
+    
+if __name__ == "__main__":
+    total_size = 0
+    status_codes = {'200': 0, '301': 0,
+                    '400': 0, '401': 0,
+                    '403': 0, '404': 0,
+                    '405': 0, '500': 0}
 
 
-try:
-    count = 0  # 行カウント
-    for line in sys.stdin:
-        count += 1
 
-        ip, request, status, size = parse_line(line)
 
-        if request == "GET /projects/260 HTTP/1.1":
-            total_size += int(size)
-            status_codes[status] += 1
+    try:
+        count = 0  # 行カウント
+        for line in sys.stdin:
+            count += 1
 
-        if count % 10 == 0:
-            print(f'File size: {total_size}')
-            for code, times in sorted(status_codes.items()):
-                if times:
-                    print(f'{code}: {times}')
-              
-except KeyboardInterrupt:
-    pass
+            ip, request, status, size = parse_line(line)
 
-finally:
-    print(f'File size: {total_size}')
-    for code, times in sorted(status_codes.items()):
-        if times:
-            print(f'{code}: {times}')
+            if request == "GET /projects/260 HTTP/1.1":
+                total_size += int(size)
+                status_codes[status] += 1
+
+            if count % 10 == 0:
+                print(f'File size: {total_size}')
+                for code, times in sorted(status_codes.items()):
+                    if times:
+                        print(f'{code}: {times}')
+                
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        print(f'File size: {total_size}')
+        for code, times in sorted(status_codes.items()):
+            if times:
+                print(f'{code}: {times}')
