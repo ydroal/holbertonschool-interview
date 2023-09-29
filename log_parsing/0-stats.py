c6c30bf165e2701d02ce0deb35c350d6e616da18
ydroal <droal.yoko@gmail.com>
2023-09-29 12:27:55 +0200

    fix: wrong format error

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -6,10 +6,18 @@ import sys
 def parse_line(line):
     """Parses a line of log input"""
     try:
+        # IPアドレスを取得
         ip = line.split(' - ')[0]
+        # "GET /projects/260 HTTP/1.1" の部分を取得
         request = line.split("\"")[1]
+        # ステータスコードとサイズを取得
         status_size = line.split()[-2:]
         status, size = status_size
+
+        if status not in status_codes:
+            return None, None, None, None
+
+        size = int(size)
 
         return ip, request, status, size
     except Exception:
@@ -29,6 +37,9 @@ if __name__ == "__main__":
             count += 1
 
             ip, request, status, size = parse_line(line)
+
+            if None in [ip, request, status, size]:
+                continue
 
             if request == "GET /projects/260 HTTP/1.1":
                 total_size += int(size)
