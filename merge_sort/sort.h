0fc282e9c42e6d04ba283210567318049dc3561c
ydroal <droal.yoko@gmail.com>
2024-02-20 16:01:10 +0100

    update: sort.h

--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -7,7 +7,7 @@
 
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void recursive_merge(int *array, int start, int end);
+void recursive_merge(int *array, int *tmp, int start, int end);
 void merge(int *array, int *tmp, int start, int mid, int end);
 
 #endif /* SORT_H */
