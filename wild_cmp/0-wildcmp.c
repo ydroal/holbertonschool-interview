6b299c604057419a06e8a8d04b63a68162feb5ef
ydroal <droal.yoko@gmail.com>
2024-04-02 16:00:29 +0200

    update: while loop removed according to task requirements

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -1,6 +1,22 @@
 #include "holberton.h"
 
 /**
+ * skip_wildcards - Skip over wildcard characters ('*') in a s2 string.
+ * @s2: Pointer to the string may containing consecutive '*' characters.
+ *
+ * Return: A pointer to the the last consecutive ('*') character in s2,
+ *         or to the '*' character if there are no consecutive '*' characters.
+ */
+
+char *skip_wildcards(char *s2)
+{
+	if (*s2 == '*' && *(s2 + 1) == '*')
+		return (skip_wildcards(s2 + 1));
+
+	return (s2);
+}
+
+/**
  * wildcmp - Compares two strings and returns 1 if the strings can be
  * considered identical, otherwise return 0.
  * @s1: The first string to compare.
@@ -26,8 +42,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		/* 文字列中にある連続する*をスキップ */
 		/* 複数の*が連続していても1つの*として扱う*/
-		while (*(s2 + 1) == '*')
-			s2++;
+		s2 = skip_wildcards(s2);
 
 		/* *が文字列末尾なら文字列は同一とみなす */
 		if (*(s2 + 1) == '\0')
